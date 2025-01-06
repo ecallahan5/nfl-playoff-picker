6d54bc8694ee416a2d50ef1b851ef03463bb195b
Eric Callahan <ecallahan5@gmail.com>
2025-01-06 10:38:06 -0500

    oops

--- pages/Live_Standings.py
+++ pages/Live_Standings.py
@@ -38,14 +38,14 @@ conf_mult = 4
 pro_mult = 1
 sb_mult = 6
 
-standings_df["wc_pts"] = np.where(standings_df["WC1"].str.strip().isin(wc_winners),wc_mult,0) + np.where(standings_df["WC2"].str.strip().isin(wc_winners),wc_mult,0) + np.where(standings_df["WC3"].str.strip().isin(wc_winners),wc_mult,0)  +\
-              np.where(standings_df["WC4"].str.strip().isin(wc_winners),wc_mult,0) + np.where(standings_df["WC5"].str.strip().isin(wc_winners),wc_mult,0) + np.where(standings_df["WC6"].str.strip().isin(wc_winners),wc_mult,0) 
-standings_df["div_pts"] = np.where(standings_df["D1"].str.strip().isin(div_winners),div_mult,0) + np.where(standings_df["D2"].str.strip().isin(div_winners),div_mult,0) +\
-                np.where(standings_df["D3"].str.strip().isin(div_winners),div_mult,0) + np.where(standings_df["D4"].str.strip().isin(div_winners),div_mult,0)
-standings_df["conf_pts"] = np.where(standings_df["AFC"].str.strip().isin(conf_winners),conf_mult,0) + np.where(standings_df["NFC"].str.strip().isin(conf_winners),conf_mult,0) 
-standings_df["pro_pts"] = np.where(standings_df["PB"].str.strip().isin(pro_winner),pro_mult,0)
-standings_df["sb_pts"] = np.where(standings_df["SB"].str.strip().isin(sb_winner),sb_mult,0)
-standings_df["Total"] = standings_df["wc_pts"] + standings_df["div_pts"] + standings_df["conf_pts"]+standings_df["pro_pts"]+standings_df["sb_pts"]
+# standings_df["wc_pts"] = np.where(standings_df["WC1"].str.strip().isin(wc_winners),wc_mult,0) + np.where(standings_df["WC2"].str.strip().isin(wc_winners),wc_mult,0) + np.where(standings_df["WC3"].str.strip().isin(wc_winners),wc_mult,0)  +\
+#               np.where(standings_df["WC4"].str.strip().isin(wc_winners),wc_mult,0) + np.where(standings_df["WC5"].str.strip().isin(wc_winners),wc_mult,0) + np.where(standings_df["WC6"].str.strip().isin(wc_winners),wc_mult,0) 
+# standings_df["div_pts"] = np.where(standings_df["D1"].str.strip().isin(div_winners),div_mult,0) + np.where(standings_df["D2"].str.strip().isin(div_winners),div_mult,0) +\
+#                 np.where(standings_df["D3"].str.strip().isin(div_winners),div_mult,0) + np.where(standings_df["D4"].str.strip().isin(div_winners),div_mult,0)
+# standings_df["conf_pts"] = np.where(standings_df["AFC"].str.strip().isin(conf_winners),conf_mult,0) + np.where(standings_df["NFC"].str.strip().isin(conf_winners),conf_mult,0) 
+# standings_df["pro_pts"] = np.where(standings_df["PB"].str.strip().isin(pro_winner),pro_mult,0)
+# standings_df["sb_pts"] = np.where(standings_df["SB"].str.strip().isin(sb_winner),sb_mult,0)
+# standings_df["Total"] = standings_df["wc_pts"] + standings_df["div_pts"] + standings_df["conf_pts"]+standings_df["pro_pts"]+standings_df["sb_pts"]
 
 st.header('Live 2025 Standings Coming Soon!')
 current_standings = standings_df[["Name", "Total"]].sort_values(by=['Total'], ascending=False).set_index(["Name"])
