c8dae927e2d79a4c8e1bfbd40af4ec9ef5f73690
Eric Callahan <ecallahan5@gmail.com>
2025-01-12 23:19:44 -0500

    commanders

--- pages/Live_Standings.py
+++ pages/Live_Standings.py
@@ -8,7 +8,7 @@ from google.oauth2 import service_account
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-wc_winners = ["4. Houston Texans", "3. Baltimore Ravens", "2. Buffalo Bills", "2. Philadelphia Eagles"]
+wc_winners = ["4. Houston Texans", "3. Baltimore Ravens", "2. Buffalo Bills", "2. Philadelphia Eagles", "6. Washington Commanders"]
 div_winners = []
 conf_winners = []
 pro_winner = []
